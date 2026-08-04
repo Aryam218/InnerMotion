f87f2e40579f89e466d448217f9d7ddf8802021b
Aryam Almutairi <aryaam871@gmail.com>
2026-08-04 16:54:42 +0300

    Initial Commit

---
+++ InnerMotion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InnerMotion
+//
+//  Created by Aryam Almutairi on 21/02/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InnerMotion/InnerMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerMotionApp.swift
+//  InnerMotion
+//
+//  Created by Aryam Almutairi on 21/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct InnerMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
