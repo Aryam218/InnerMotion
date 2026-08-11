db1f68f27d0fa39ebc4dff4bb7e57a36ae73ce18
saba alzaqzouq <sabahashem221w@gmail.com>
2026-08-11 09:18:52 +0300

    fixed the font and the button making it stander cross all the pages

---
+++ InnerMotion/Typography.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Font {
+    static let appLargeTitle = Font.system(size: 36, weight: .regular, design: .rounded)
+    static let appTitle = Font.system(size: 28, weight: .bold, design: .rounded)
+    static let appSubtitle = Font.system(size: 18, weight: .regular, design: .rounded)
+    static let appSectionTitle = Font.system(size: 18, weight: .bold, design: .rounded)
+    static let appBody = Font.system(size: 16, weight: .regular, design: .rounded)
+    static let appBodyMedium = Font.system(size: 16, weight: .medium, design: .rounded)
+    static let appButton = Font.system(size: 17, weight: .semibold, design: .rounded)
+    static let appSmall = Font.system(size: 14, weight: .regular, design: .rounded)
+    static let appSmallMedium = Font.system(size: 14, weight: .medium, design: .rounded)
+    static let appCaption = Font.system(size: 12, weight: .regular, design: .rounded)
+}
